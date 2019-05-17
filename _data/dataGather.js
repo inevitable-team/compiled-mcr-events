3144674b1bb0594dcef3050d65fb074989bea9d1
Sean O'Mahoney <seandavidomahoney@gmail.com>
2019-05-17 10:52:26 +0100

    init

---
+++ _data/dataGather.js
@@ -0,0 +1,9 @@
+let meetup = require("./meetup"),
+    eventbrite = require("./eventbrite"),
+    googleCalendar = require("./googleCalendar");
+
+class dataGather {
+
+}
+
+module.exports = dataGather;
---
+++ script.js
@@ -0,0 +1,4 @@
+let fs = require('fs'),
+    shell = require('shelljs');
+
+shell.mkdir('-p', './_site');
