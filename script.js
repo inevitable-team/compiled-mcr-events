676df1e7b88558792dcd7c6ea96978ed13198e1b
Sean O'Mahoney <seandavidomahoney@gmail.com>
2019-05-20 17:32:41 +0100

    Meetup Groups & Testing

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 let fs = require('fs'),
-    shell = require('shelljs');
+    shell = require('shelljs'),
+    beautify = require('beautify'),
+    dataGatherClass = require("./_data/dataGather");
 
 shell.mkdir('-p', './_site');
+
+// TESTING
+shell.mkdir('-p', './_exports');
+let dataGather = new dataGatherClass();
+dataGather.getData().then(data => {
+    fs.writeFileSync(`./_exports/groups`, beautify(data[0], {format: 'json'}), () => {});
+    fs.writeFileSync(`./_exports/events`, beautify(data[1], {format: 'json'}), () => {});
+});
+
