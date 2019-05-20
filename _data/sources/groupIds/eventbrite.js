9f491284cef75b456e5e75e1d851b67fc54ab3ae
Sean O'Mahoney <seandavidomahoney@gmail.com>
2019-05-20 17:23:43 +0100

    Eventbrite Groups

--- _data/sources/groupIds/eventbrite.js
+++ _data/sources/groupIds/eventbrite.js
@@ -2,25 +2,95 @@ module.exports = [{
     id: 5881267695,
     name: "The Landing @ MediaCityUK",
     img: "https://cdn.evbuc.com/images/33140634/57365423599/2/logo.jpg",
-    url: ""
+    url: "https://www.eventbrite.co.uk/o/the-landing-mediacityuk-5881267695"
 }, {
     id: 17108698390,
     name: "Manchester Java Community",
     img: "",
-    url: ""
+    url: "https://www.eventbrite.co.uk/o/manchester-java-community-17108698390"
 }, {
-    id: 17108698390,
-    name: "",
-    img: "",
-    url: ""
+    id: 4493390085,
+    name: "Ladies That UX Manchester",
+    img: "https://cdn.evbuc.com/images/12148173/38866758074/1/logo.jpg",
+    url: "https://www.eventbrite.co.uk/o/ladies-that-ux-manchester-4493390085"
 }, {
-    id: 17108698390,
-    name: "",
+    id: 17831433367,
+    name: "The Landing",
     img: "",
-    url: ""
+    url: "https://www.eventbrite.co.uk/o/the-landing-17831433367"
 }, {
-    id: 17108698390,
-    name: "",
+    id: 9036612572,
+    name: "Liberty Apps",
     img: "",
-    url: ""
+    url: "https://www.eventbrite.co.uk/o/liberty-apps-9036612572"
+}, {
+    id: 227118780,
+    name: "Manchester Digital",
+    img: "https://cdn.evbuc.com/images/64712/17518308731/1/logo.png",
+    url: "https://www.eventbrite.com/o/manchester-digital-227118780"
+}, {
+    id: 16693829495,
+    name: "LambdaLounge",
+    img: "",
+    url: "https://www.eventbrite.com/o/lambdalounge-16693829495"
+}, {
+    id: 17182493364,
+    name: "e3creative",
+    img: "",
+    url: "https://www.eventbrite.co.uk/o/e3creative-17182493364"
+}, {
+    id: 16890786611,
+    name: "InnovateHer",
+    img: "",
+    url: "https://www.eventbrite.co.uk/o/innovateher-16890786611"
+}, {
+    id: 19702492738,
+    name: "Code and Stuff",
+    img: "https://cdn.evbuc.com/images/58091423/296630288170/2/logo.png",
+    url: "https://www.eventbrite.co.uk/o/code-and-stuff-19702492738"
+}, {
+    id: 17926789218,
+    name: "Happy Hour Group Limited",
+    img: "https://cdn.evbuc.com/images/50684026/252175942874/1/logo.png",
+    url: "https://www.eventbrite.co.uk/o/happy-hour-group-limited-17926789218"
+}, {
+    id: 17691796705,
+    name: "Faculty of Science and Engineering",
+    img: "",
+    url: "https://www.eventbrite.co.uk/o/faculty-of-science-and-engineering-17691796705"
+}, {
+    id: 17544573305,
+    name: "The Federation",
+    img: "",
+    url: "https://www.eventbrite.co.uk/o/the-federation-17544573305"
+}, {
+    id: 14858329308,
+    name: "Full Stack of Pancakes",
+    img: "https://cdn.evbuc.com/images/49516790/222933883358/2/logo.png",
+    url: "https://www.eventbrite.com/o/full-stack-of-pancakes-14858329308"
+}, {
+    id: 12865612253,
+    name: "Northern Links Networking",
+    img: "",
+    url: "https://www.eventbrite.com/o/northern-links-networking-12865612253"
+}, {
+    id: 14622313683,
+    name: "Lloyds Bank",
+    img: "https://cdn.evbuc.com/images/34930367/220280744486/2/logo.jpg",
+    url: "https://www.eventbrite.co.uk/o/lloyds-bank-14622313683"
+}, {
+    id: 8191993336,
+    name: "Tech Circus Meetups",
+    img: "https://cdn.evbuc.com/images/54341136/143816548817/2/logo.png",
+    url: "https://www.eventbrite.co.uk/o/tech-circus-meetups-8191993336"
+}, {
+    id: 7227143335,
+    name: "SheSays MCR",
+    img: "",
+    url: "shesays-mcr-7227143335"
+}, {
+    id: 11845832416,
+    name: "MancML",
+    img: "https://cdn.evbuc.com/images/36331831/175743644937/2/logo.png",
+    url: "https://www.eventbrite.co.uk/o/mancml-11845832416"
 }];
