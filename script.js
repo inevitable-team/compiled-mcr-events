1c530d1acac45ad8d14b4fad8df908624b0f0622
Sean O'Mahoney <seandavidomahoney@gmail.com>
2019-05-23 17:16:23 +0100

    Images Local

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let fs = require('fs-extra'),
     shell = require('shelljs'),
     beautify = require('beautify'),
+    Path = require('path'),
+    Axios = require('axios'),
     dataGatherClass = require(`${__dirname}/_data/dataGather`),
     htmlConverter = require(`${__dirname}/_static/scripts/dataToHTML`),
     indexLayout = require(`${__dirname}/_layout/index`);
@@ -14,15 +16,44 @@ fs.copySync(`${__dirname}/_static`, `${__dirname}/_site`);
 shell.mkdir('-p', `${__dirname}/_exports`);
 shell.mkdir('-p', `${__dirname}/_site/data`);
 let dataGather = new dataGatherClass();
+
 dataGather.getData().then(data => {
-    // TESTING
-    fs.writeFileSync(`${__dirname}/_exports/groups.json`, beautify(JSON.stringify(data[0]), {format: 'json'}), () => {});
-    fs.writeFileSync(`${__dirname}/_exports/events.json`, beautify(JSON.stringify(data[1]), {format: 'json'}), () => {});
-    // Output
-    fs.writeFileSync(`${__dirname}/_site/data/groups.json`, beautify(JSON.stringify(data[0]), {format: 'json'}), () => {});
-    fs.writeFileSync(`${__dirname}/_site/data/events.json`, beautify(JSON.stringify(data[1]), {format: 'json'}), () => {});
-    // Creating HTML
-    let eventsHTML = data[1].map(htmlConverter.eventHTML).join(""), groupsHTML = data[0].map(htmlConverter.groupHTML).join("");
-    let index = indexLayout(eventsHTML, groupsHTML);
-    fs.writeFileSync(`${__dirname}/_site/index.html`, index, () => {});
+    Promise.all(data[0].map(downloadImage)).then().then(groupsWithLocalImages => {
+        data[0] = groupsWithLocalImages;
+        // Got data and images
+        fs.writeFileSync(`${__dirname}/_exports/groups.json`, beautify(JSON.stringify(data[0]), { format: 'json' }), () => {});
+        fs.writeFileSync(`${__dirname}/_exports/events.json`, beautify(JSON.stringify(data[1]), { format: 'json' }), () => {});
+        // Output
+        fs.writeFileSync(`${__dirname}/_site/data/groups.json`, beautify(JSON.stringify(data[0]), { format: 'json' }), () => {});
+        fs.writeFileSync(`${__dirname}/_site/data/events.json`, beautify(JSON.stringify(data[1]), { format: 'json' }), () => {});
+        // Creating HTML
+        let eventsHTML = data[1].map(htmlConverter.eventHTML).join(""),
+            groupsHTML = data[0].map(htmlConverter.groupHTML).join("");
+        let index = indexLayout(eventsHTML, groupsHTML);
+        fs.writeFileSync(`${__dirname}/_site/index.html`, index, () => {});
+    });
 });
+
+async function downloadImage(group) {
+    if (!group.img.includes("http")) return group; // If not image on the internet
+    const url = group.img,
+    name = group.img.split("/").pop(),
+    path = Path.resolve(__dirname, '_static/img', name),
+    writer = fs.createWriteStream(path);
+
+    const response = await Axios({
+        url,
+        method: 'GET',
+        responseType: 'stream'
+    });
+
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+        writer.on('finish', () => {
+            group.img = `./img/${name}`;
+            resolve(group);
+        })
+        writer.on('error', () => resolve(group))
+    })
+}
