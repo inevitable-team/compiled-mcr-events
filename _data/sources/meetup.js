bc0671f1059306c5fc69e6350e185823b76e2f58
seirra blake <sophietheopossum@yandex.ru>
2021-10-04 15:52:19 +0100

    no more fetch

--- _data/sources/meetup.js
+++ _data/sources/meetup.js
@@ -103,7 +103,7 @@ class meetup {
         return new Promise(resolve => {
             Promise.all(this.groups.map((groupId, i) => new Promise(resolve => setTimeout(() => resolve(groupId), i*400)).then(groupId => {
               console.log('Fetching group details for: ', groupId);
-              return fetch(this.apiGroup(groupId), this.header);
+              return graphQLClient.request(this.apiGroup(groupId));
             }))).then(responses =>
                 Promise.all(responses.map(res => res.text()))
             ).then(texts => {
@@ -117,7 +117,7 @@ class meetup {
         return new Promise(resolve => {
             Promise.all(this.groups.map((groupId, i) => new Promise(resolve => setTimeout(() => resolve(groupId), i*400)).then(groupId => {
               console.log('Fetching events for: ', groupId);
-              return fetch(this.apiEvents(groupId), this.header);
+              return graphQLClient.request(this.apiEvents(groupId));
             }))).then(responses =>
                 Promise.all(responses.map(res => res.text()))
             ).then(texts => {
