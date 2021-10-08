c3c5078de7c3902d729bc8b99371dbc31ab8618d
seirra blake <sophietheopossum@yandex.ru>
2021-10-08 14:49:04 +0100

    remove trailing whitespace

--- _data/sources/meetup.js
+++ _data/sources/meetup.js
@@ -34,7 +34,7 @@ class meetup {
                         }
                     }
                     name
-                    link 
+                    link
                 }
             }
         `;
