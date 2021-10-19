f17610ac9d4a0dbdd5808840a9526ea7cbe98a31
seirra blake <sophietheopossum@yandex.ru>
2021-10-19 14:48:28 +0100

    a [topic] can have a little [null] as a [treat]

--- _data/sources/meetup.js
+++ _data/sources/meetup.js
@@ -88,7 +88,7 @@ class meetup {
                     false,
                     event.isOnline,
                     ! event.isOnline,
-                    [event.topics.name]
+                    [event.topics.name] || [null]
                 );
         };
     }
