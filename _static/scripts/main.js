c0fe02d1137d70e0dba5d6d7efa1b5b6411332fe
Sean O'Mahoney <seandavidomahoney@gmail.com>
2022-12-06 10:49:02 +0000

    Implemented Initial ToastUI Calendar

--- _static/scripts/main.js
+++ _static/scripts/main.js
@@ -1,8 +1,9 @@
-const nn = (txt) => (txt || "").toLowerCase().replace(/[^a-z0-9]/gi,'');
-let events = [], groups = [], eventsSearchElement, groupsSearchElement;
+const nn = (txt) => (txt || "").toLowerCase().replace(/[^a-z0-9]/gi, '');
+let events = [], groups = [], toastUiEvents = [], eventsSearchElement, groupsSearchElement;
+let todayButton, prevButton, nextButton, range, view, Calendar, container, calendar;
 
 window.addEventListener("load", function () {
-    
+
     checkUrl();
 
     apsc(document.getElementsByClassName('type')).forEach((t, i) => {
@@ -23,6 +24,12 @@ window.addEventListener("load", function () {
 
     eventsSearchElement = document.getElementById("eventsSearchBox");
     groupsSearchElement = document.getElementById("groupsSearchBox");
+    todayButton = document.getElementById('today');
+    prevButton = document.getElementById('prev');
+    nextButton = document.getElementById('next');
+    range = document.getElementById('navbar--range');
+    view = document.getElementById('view-selection');
+    setupToastCalendar();
 
     groupsSearchElement.addEventListener("keyup", searchGroups);
     document.getElementById("groupsClearSearch").addEventListener("click", clearGroupsSearch);
@@ -30,15 +37,114 @@ window.addEventListener("load", function () {
     eventsSearchElement.addEventListener("keyup", searchEvent);
     document.getElementById("eventsClearSearch").addEventListener("click", clearEventSearch);
 
+    todayButton.addEventListener('click', function () {
+        calendar.today();
+        displayEvents();
+        displayRenderRange();
+    });
+
+    prevButton.addEventListener('click', function () {
+        calendar.prev();
+        displayEvents();
+        displayRenderRange();
+    });
+
+    nextButton.addEventListener('click', function () {
+        calendar.next();
+        displayEvents();
+        displayRenderRange();
+    });
+
+    view.addEventListener('change', function () {
+        calendar.changeView(this.value);
+        displayEvents();
+        displayRenderRange();
+    })
+
     let url = window.location.href.replace("index.html", "");
 
     getData(`${url}data/events.json`, data => {
         events = data;
-    })
+        toastUiEvents = mapToastUiEvents(data);
+        displayEvents();
+        displayRenderRange();
+        setDefaultView(view);
+    });
+
     getData(`${url}data/groups.json`, data => {
         groups = data;
-    })
+    });
 });
+
+function setupToastCalendar() {
+    Calendar = tui.Calendar;
+    container = document.getElementById("calendar");
+    calendar = new Calendar(container, {
+        defaultView: "month",
+        useDetailPopup: true,
+        isReadOnly: true,
+        calendars: [
+            {
+                id: "Meetup",
+                name: "Meetup",
+            },
+            {
+                id: "Eventbrite",
+                name: "Eventbrite",
+            },
+            {
+                id: "TechNW",
+                name: "TechNW",
+            },
+            {
+                id: "Manchester Tech Events",
+                name: "Manchester Tech Events",
+            },
+        ],
+        week: {
+            startDayOfWeek: 1,
+            taskView: false,
+        },
+        month: {
+            startDayOfWeek: 1
+        },
+    });
+}
+
+function mapToastUiEvents(events) {
+    return events.map((event, index) => {
+        return {
+            id: index,
+            calendarId: event.source,
+            title: event.name,
+            body: `
+            <br/>
+            <a href="${event.link}" target="_blank">🖇️ Sign-up Link 🖇️</a>
+            <br/>
+            <br/>
+            ${event.desc}`,
+            location: event.location,
+            start: event.startTimeISO,
+            end: event.endTimeISO,
+            isReadOnly: true,
+            backgroundColor: "#7eb7e7"
+        }
+    })
+}
+
+function displayEvents() {
+    calendar.clear();
+    calendar.createEvents(toastUiEvents);
+}
+
+function displayRenderRange() {
+    range.textContent = getNavbarRange(calendar.getDateRangeStart(), calendar.getDateRangeEnd(), view.value);
+}
+
+function setDefaultView(viewElement) {
+    viewElement.value = window.innerWidth > 600 ? "month" : "day";
+    calendar.changeView(viewElement.value);
+}
 
 function searchGroups() {
     if (groupsSearchElement.value.length == 0) { clearGroupsSearch(); return; }
@@ -86,6 +192,24 @@ function getData(url, callback) {
     };
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
+}
+
+function getNavbarRange(tzStart, tzEnd, viewType) {
+    var start = tzStart.toDate();
+    var end = tzEnd.toDate();
+    var middle;
+    if (viewType === 'month') {
+        middle = new Date(start.getTime() + (end.getTime() - start.getTime()) / 2);
+
+        return moment(middle).format('MMM YYYY');
+    }
+    if (viewType === 'day') {
+        return moment(start).format('DD MMM YYYY');
+    }
+    if (viewType === 'week') {
+        return moment(start).format('DD MMM YYYY') + ' ~ ' + moment(end).format('DD MMM YYYY');
+    }
+    throw new Error('no view type');
 }
 
 function apsc(e) {
